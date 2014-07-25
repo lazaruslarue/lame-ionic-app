3fa029ea17946cfa84f0744ef8c565cd47a83b50
Jesse F. LaRue <lazaruslarue@gmail.com>
2014-07-25 13:33:43 -0700

    Adjust to support ionic server
    using ````../*```` breaks serving by ````ionic serve````

--- www/js/states/state.one.js
+++ www/js/states/state.one.js
@@ -2,6 +2,6 @@ angular.module('state.one',[]).config( function ($stateProvider) {
   $stateProvider.state("one", {
     url: "/one",
     controller: "ExampleCtrl",
-    templateUrl: "../templates/state.one.html",
+    templateUrl: "templates/state.one.html",
   })
 });
